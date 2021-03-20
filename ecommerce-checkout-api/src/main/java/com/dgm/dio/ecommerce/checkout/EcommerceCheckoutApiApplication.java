887e2d7172f3bfd98e42020b5214872a8316e3f3
Danilo <danilogmoura@gmail.com>
2021-03-20 05:27:20 -0300

    Initial commit

---
+++ ecommerce-checkout-api/src/main/java/com/dgm/dio/ecommerce/checkout/EcommerceCheckoutApiApplication.java
@@ -0,0 +1,13 @@
+package com.dgm.dio.ecommerce.checkout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceCheckoutApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceCheckoutApiApplication.class, args);
+	}
+
+}
